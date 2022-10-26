fc5f07f0fd03c234974ed8b73e1493b98e2270a5
Tomasz <tomaszmikulka@gmail.com>
2022-10-26 20:18:13 +0200

    obiektowosc

---
+++ src/Rabbit.java
@@ -0,0 +1,18 @@
+public class Rabbit {
+    private int length;
+    private String name;
+    private boolean softness;
+
+    Rabbit(int length, String name, boolean softness)
+    {
+        this.length = length;
+        this.name = name;
+        this.softness = softness;
+    }
+    public void changeName(String name){
+        this.name = name;
+    }
+    public String showName(){
+        return name;
+    }
+}
--- src/program1.java
+++ src/program1.java
@@ -3,5 +3,10 @@ public class program1 {
 
         Car auto1 = new Car(4,5,6,"czerwony");
         Car auto2 = new Car(4,4,2,"niebieski");
+        Rabbit krolik1 = new Rabbit(50,"Hanek",true);
+        krolik1.changeName("Stasiek");
+        krolik1.showName();
+        System.out.println(krolik1.showName());
+
     }
 }

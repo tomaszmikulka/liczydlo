9273e0deb2c6ddd981ab7287193ea2a49e1e26d7
Tomasz <tomaszmikulka@gmail.com>
2022-10-26 19:06:05 +0200

    zadanie2

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    int wheels;
+    int seats;
+    int lights;
+    String colour;
+    Car(int wheels, int seats, int lights, String colour){
+        this.wheels = wheels;
+        this.seats = seats;
+        this.lights = lights;
+        this.colour = colour;
+    }
+}
---
+++ src/program1.java
@@ -0,0 +1,7 @@
+public class program1 {
+    public static void main(String[] args) {
+
+        Car auto1 = new Car(4,5,6,"czerwony");
+        Car auto2 = new Car(4,4,2,"niebieski");
+    }
+}

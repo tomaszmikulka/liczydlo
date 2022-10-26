559ad03656275283c3a8a3ce8c7d654df8773dea
Tomasz <tomaszmikulka@gmail.com>
2022-10-26 17:53:37 +0200

    zadanie trzecie

--- src/liczydlo.java
+++ src/liczydlo.java
@@ -2,8 +2,8 @@ public class liczydlo {
     public static void main(String[] args) {
         int [] liczydlo=new int[] {2,3,7,3,3,5,2,3,3,3};
         int count=0;
-        for (int cyfry: liczydlo) {
-            if (cyfry==3) {
+        for (int cyfra: liczydlo) {
+            if (cyfra==3) {
                 count++;
             }
         }
